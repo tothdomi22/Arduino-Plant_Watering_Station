c1c60231323bdb4952c924af2d38566041488679
PeterJanosi <92621566+PeterJanosi@users.noreply.github.com>
2021-10-22 13:59:52 +0200

    Create moisture_data.php

---
+++ Weblap/chartjs/moisture_data.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');
+
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', '*******'); //database username
+define('DB_PASSWORD', '********'); //database jelszó
+define('DB_NAME', '********'); //batabase név
+
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if(!$mysqli){
+  die("Connection failed: " . $mysqli->error);
+}
+
+$query = sprintf("SELECT reading_time, moisture FROM SensorData");
+
+$result = $mysqli->query($query);
+
+$data = array();
+foreach ($result as $row) {
+  $data[] = $row;
+}
+
+$result->close();
+
+$mysqli->close();
+
+print json_encode($data);
